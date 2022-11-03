9980dc7d64762213680dfe12b77b6af5ea9ad12a
nathaliabrazk <nathaliabrazk@gmail.com>
2022-11-03 18:54:14 -0300

    Update Projeto 1.c

--- PROJETOS/Projeto 1.c
+++ PROJETOS/Projeto 1.c
@@ -1,14 +1,13 @@
 /* com OK está pronto:
 Utilizando os conceitos de vetor, implemente um algoritmo que:
-inclua até 1000 usuários; OK
-edite um usuário;
-exclua um usuário;
-busque um usuário pelo email;
-imprima todos os usuários cadastrados - ok case 5 pronto com bug;
-faça backup dos usuários cadastrados;
-faça restauração dos dados;
+1-inclua até 1000 usuários; OK mas com bug na validacao do sexo
+2-edite um usuário; OK mas com bug na validacao do sexo
+3-exclua um usuário;falta fazer
+4-busque um usuário pelo email;erro nas variaveis
+5-imprima todos os usuários cadastrados - ok case 5 pronto com bug
+6-faça backup dos usuários cadastrados;erro nas variaveis
+7-faça restauração dos dados;falta fazer
 
-Dados do usuário:
 Id (int) => preenchido automaticamente por números randômicos
 Nome completo (string)
 Email (string) => validação do campo: verificar se o caractere "@" aparece
@@ -24,23 +23,24 @@ Vacina (tomou a vacina? boolean)*/
 
 int main (){
     //declaracao de variaveis
-	int i=0, id[1000];
+	int i=0,id[1000];
 	char nome[1000][50], email[1000][50], sexo[1000][15], endereco[1000][50], emailBusca[1000][50]; //tipo %s
 	double altura[1000]; //%lf
 	char vacina [1000][4]; //%s
 	int menu;
-	int x, j, excluir;
+	int x, j, excluir;//x=case 5 para imprimir dados
 		//impressao de menu e escolha de case	
 	do {
-		printf("Digite a opcao desejada do menu:\n");
-		printf ("1 - Cadastrar um usuario \n");
-		printf ("2 - Editar cadastro \n");
-		printf ("3 - Excluir cadastro \n");
-		printf ("4 - Buscar pelo email \n");
-		printf ("5 - Imprimir todos usuarios cadastrados \n");
-		printf ("6 - Realizar backup \n");
-		printf ("7 - Restaurar dados \n");
-        printf("8  - Fechar o programa\n");
+		printf("----------MENU----------\n\n");
+		printf("1 - Cadastrar um usuario \n");
+		printf("2 - Editar cadastro \n");
+		printf("3 - Excluir cadastro \n");
+		printf("4 - Buscar pelo email \n");
+		printf("5 - Imprimir todos usuarios cadastrados \n");
+		printf("6 - Realizar backup \n");
+		printf("7 - Restaurar dados \n");
+        printf("8 - Fechar o programa\n\n");
+        printf("Digite a opcao desejada: ");
 		fflush(stdin);
 		scanf("%i", &menu);
 				
@@ -48,7 +48,7 @@ int main (){
         //case 1-Cadastro de usuario
 		case 1:
 	    id[i] = rand() %5000;
-	    printf("Usuario:%d\n ID:%d\n", i +1,id[i]); // i+1 para saber em qual cadastro estar.
+	    printf("Usuario:%d\nID:%d\n", i +1,id[i]); // i+1 para saber em qual cadastro esta.
 	    
 		printf("Digite seu nome: ");
 		fflush(stdin);
@@ -58,61 +58,63 @@ int main (){
 		printf("Digite seu email: ");
 		scanf("%s", email[i]);
 		getchar();
-        printf("\n");
+
 		
 		if(strchr(email[i], '@') == 0){
 			printf("Email invalido\n");
 			
-			printf("Forneca o email: ");
+			printf("Forneca um email valido contendo @: ");
 	 		fflush(stdin);
 	 		fgets(email[i],50,stdin);
 		}
 		else{
-			printf("Email valido\n");
+			printf("Email valido\n\n");
 		}
 		
-		printf(" Digite seu endereco: \n");
+		printf("Digite seu endereco: ");
 		fflush(stdin);
 		fgets(endereco[i],50,stdin);
 		printf("\n");
 
-		printf("Informe seu sexo (Feminino, Masculino e Não declarar):");//arrumar bug na validacao
+		printf("Informe seu sexo (Feminino, Masculino ou Nao declarar):");//arrumar bug na validacao
             fflush(stdin);
             fgets(sexo[i],50,stdin);
             
-            if(strcmp(sexo[i], "Feminino") == 0 && strcmp(sexo[i], "Masculino") == 0 && strcmp(sexo[i], "Feminino") == 0){
-                printf("Sexo invalido\n");//nao esta imprimindo sexo valido nem invalido
+            if(strcmp(sexo[i], "Feminino") == 0 || strcmp(sexo[i], "Masculino") == 0 || strcmp(sexo[i], "Feminino") == 0){
+                printf("Sexo invalido\n\n");//nao esta imprimindo sexo valido nem invalido
             }else{
-                printf("Sexo valido");
+                printf("Sexo valido\n\n");
             }
-            break;
 
-		printf("Digite a altura: \n");
+		printf("Digite a altura: ");
 		scanf("%lf", &altura[i]);
 		
-		if(altura[i] > 1.00 && altura[i] < 2.00){//arrumar validacao de altura
+		if(altura[i] > 1.00 && altura[i] < 2.00){
 			printf("Altura valida\n");	
 		 }
 		 else{
 		 	printf("Altura invalida\n");
-		
-		} 
+			printf("Digite uma altura valida: ");
+			fflush(stdin);
+			scanf("%lf", &altura[i]);
+			printf("\n");
+		}
 				
-		printf("Digite sim se voce foi vacinado ou nao, caso nao tenha sido: \n");
+		printf("Digite sim se voce foi vacinado ou nao, caso nao tenha sido: ");
 		fflush(stdin);
 		scanf("%s", &vacina[i]);
 					
 		if (strcmp(vacina[i], "sim")==0){// o strcmp é utilizado para comparar uma string. "string comper"
-			printf("voce tomou a vacina.\n");		
+			printf("Voce tomou a vacina!\n\n");		
 		}
 		else{
-			printf("voce nao tomou a vacina.\n");
+			printf("Voce nao tomou a vacina!\n\n");
 		}
 		i++;
 		break;
 
         //case 2-editar algum dado de registro
-            case 2: // esta em forma de switch case precisa ser ajustado para a estrutura adequada
+            case 2: // completo mas com bug na validacao de sexo
             int editar;
                 
             int i=0, id[1000];
@@ -120,82 +122,101 @@ int main (){
             double altura[1000]; //%lf
             char vacina [1000][4]; 
             int menu;
-            printf("Deseja editar algum usuario? Digite 1 para sim ou 2 para nao. ");
+            printf("Deseja editar algum usuario? Digite 1 para sim ou 2 para nao:  ");
             fflush(stdin);
             scanf("%d", &editar);
         
             if(editar == 1){
             int index, opcao;
-            printf("Digite o index que você deseja editar: ");
+            printf("Digite o index que voce deseja editar: ");
             fflush(stdin);
             scanf("%d", &index); 
-
-            printf("Qual informacao do cadastro voce quer alterar ?\n1: Nome\n2: Email\n3: Sexo\n4: Endereco\n5: Altura\n6: Vacina\n\nDigite o numero: ");
+            
+            
+			printf("\n----------MENU DE ALTERACAO----------\n");
+			printf("10 - Alterar nome\n");
+			printf("11 - Alterar Email\n");
+			printf("12 - Alterar Sexo\n");
+			printf("13 - Alterar Endereco\n");
+			printf("14 - Alterar Altura\n");
+			printf("15 - Alterar status de vacina\n");
+            printf("Digite a alteracao que deseja fazer: ");
             fflush(stdin);
             scanf("%d", &opcao);
             switch (opcao)
             {
-            case 1:
+            case 10:
                 printf("Alterar nome:  ");
                 fflush(stdin);
                 fgets(nome[index],50,stdin);
                 break;
-            
-            case 2:
-                printf("Alterar email: ");
-                fflush(stdin);
-                fgets(email[index],50,stdin);
-                
-                printf("\n%s", email[i]);
-            
-            if(strchr(email[index], '@') == 0){
-                printf("Email invalido\n");
-                
-                printf("Alterar email:");
-                fflush(stdin);
-                fgets(email[index],50,stdin);
-            }
-            else{
-                printf("Email valido\n");
-            }
-
-                break;
-            
-            case 3:
-                printf("Alterar sexo: \n");
-                fflush(stdin);
-                fgets(sexo[index],15,stdin);
-                break;
-                
-                case 4: 
-                printf("Atualizar endereco: \n ");
-                fflush(stdin);
-                fgets(endereco[index],50,stdin);
-                break;
-                
-                    case 5 : 
-                    printf("Alterar altura: \n ");
-                    scanf("%lf", &altura[i]);
-                
-                case 6: 
-                printf("Alterar status de vacina: \n");
-                    fflush(stdin);
-            scanf("%s", &vacina[i]);
-                if (strcmp(vacina[i], "sim")== 0){// o strcmp é utilizado para comparar uma string. "string comper"
-                printf("voce tomou a vacina.\n");		
-            }
-            else{
-                printf("voce nao tomou a vacina.\n");
-            }
-            i++;
-            break;	 	
+	            
+	            case 11:
+	                printf("Alterar email: ");
+	                fflush(stdin);
+	                fgets(email[index],50,stdin);
+	                
+	                printf("\n%s", email[i]);
+	            
+	            if(strchr(email[index], '@') == 0){
+	                printf("Email invalido\n");
+	                
+	                printf("Alterar email: ");
+	                fflush(stdin);
+	                fgets(email[index],50,stdin);
+	            }
+	            else{
+	                printf("Email valido\n");
+	            }
+	
+	                break;
+		            
+	            case 12:
+	                printf("Alterar sexo: ");
+	                fflush(stdin);
+	                fgets(sexo[index],15,stdin);
+	                
+	                break;
+		                
+		                case 13: 
+		                printf("Alterar endereco: ");
+		                fflush(stdin);
+		                fgets(endereco[index],50,stdin);
+		                break;
+		                
+		                    case 14 : 
+		                    printf("Alterar altura: ");
+		                    scanf("%lf", &altura[i]);
+		                    if(altura[i] > 1.00 && altura[i] < 2.00){//arrumar validacao de altura
+							printf("Altura valida\n");	
+		 					}
+		 					else{
+		 					printf("Altura invalida\n");
+							printf("Digite uma altura valida: ");
+							fflush(stdin);
+							scanf("%lf", &altura[i]);
+							printf("\n");
+							}
+			                
+				                case 15: 
+				                printf("Alterar status de vacina: ");
+				                fflush(stdin);
+				           		scanf("%s", &vacina[i]);
+				                if (strcmp(vacina[i], "sim")== 0)
+								{
+								printf("Voce tomou a vacina!\n");		
+									}
+						            else{
+						                printf("Voce nao tomou a vacina!\n");
+						            }
+						            i++;
+						            break;	 	
         
         }
             }
             //case 3-excluir algum dado de registro do usuario
-            case 3://incompleto
+            /*case 3://incompleto
             int excluir;
-            int j;
             Printf("qual index do usuario que sera excluido? ");
             Scanf("%d",&excluir);
             for(j = excluir; j != i; j++){
@@ -206,7 +227,7 @@ int main (){
             strcpy(endereco[j], endereco[j + 1]);
             altura[j] = altura[j + 1];
             //vacina[j] = vacina[j + 1];//
-        } 
+        } */
                 //case 4-buscar por email
                 case 4: //incompleto
             
@@ -215,32 +236,44 @@ int main (){
                 for(i=0;i<i;i++){
                 
                 if(strcmp(email[i], emailBusca[i])==0){
-                printf("/ID:%d\n Nome:%s\n Email:%s\n Endereco:%s\n Sexo:%s\n Altura:%.2lf\n Vacinado:%s\n", id[x], nome[x], email[x], endereco [x], sexo[x], altura[x], vacina[x]);
+                printf("ID:%d\n",id[x]);
+                printf("Email:%s\n",email[x]);
+                printf("Nome:%s\n",nome[x]);
+                printf("Endereco:%s\n",endereco [x]);
+                printf("Sexo:%s\n",sexo[x]);
+                printf("Altura:%.2lf\n",altura[x]);
+                printf("Vacinado:%s\n",vacina[x]);
                 fflush(stdin);
                 break;
         }
         }
         
-                    // case 5-imprimir usuarios cadastrados
-                    case 5: //incompleto
-                    printf("Usuarios cadastrados:\n");
+                    // case 5-imprimir toos os usuarios cadastrados
+                    /*case 5: //erro na variavel
+                    printf("----------Usuarios cadastrados----------\n");
+        
+                    for (x=0; x<=i; x++){
+                    printf("ID:%d\n",id[x]);
+                    printf("Email:%s\n",email[x]);
+                    printf("Nome:%s\n",nome[x]);
+                    printf("Endereco:%s\n",endereco [x]);
+                    printf("Sexo:%s\n",sexo[x]);
+                    printf("Altura:%.2lf\n",altura[x]);
+                    printf("Vacinado:%s\n",vacina[x]);
                     
-                    for (x=0; x<i; x++){
-                    printf("ID:%d\n Nome:%s\n Email:%s\n Endereco:%s\n Sexo:%s\n Altura:%.2lf\n Vacinado:%s\n", id[x], nome[x], email[x], endereco [x], sexo[x], altura[x], vacina[x]);
                     fflush(stdin);
-                    break;
-                }
+                    break;*/
+                
                         //case 6-para realizar backup de registros
                         case 6://incompleto
                         printf("Realizar backup\n");
-
+							
+							//restaurar dados
                             case 7://incompleto
                             printf("Restaurar dados\n");
                 }
         
-    }while(menu!=8);
-    
+}while(menu!=8);
 
-return 0; 
 }
-		
+
