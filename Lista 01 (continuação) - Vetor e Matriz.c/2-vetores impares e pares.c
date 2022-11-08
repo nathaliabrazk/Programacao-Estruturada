4a1b5fb09d4fde7dd79552906bc6abdd672a6e20
nathaliabrazk <nathaliabrazk@gmail.com>
2022-11-08 19:05:02 -0300

    .

---
+++ Lista 01 (continuação) - Vetor e Matriz.c/2-vetores impares e pares.c
@@ -0,0 +1,12 @@
+/*Crie um programa em C que leia 10 números inteiros e armazene em um vetor
+v. Declare dois novos vetores v1 e v2. Copie os valores ímpares de v para v1,
+e os valores pares de v para v2. Note que cada um dos vetores v1 e v2 podem
+ter no máximo 10 elementos, mas nem todas as posições do vetor são
+utilizadas. No final escreva os elementos que foram armazenados em v1 e v2*/
+#include<stdio.h>
+int main(){
+//declaracao de variaveis
+int v[10],v1[10],v2[10];
+
+
+}//fim da main
