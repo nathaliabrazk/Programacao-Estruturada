8dfd436155eb22df44923870bf956cda27eca8e5
nathaliabrazk <nathaliabrazk@gmail.com>
2022-11-09 19:19:40 -0300

    .

--- Lista 01 (continuação) - Vetor e Matriz.c/2-vetores impares e pares.c
+++ Lista 01 (continuação) - Vetor e Matriz.c/2-vetores impares e pares.c
@@ -6,7 +6,38 @@ utilizadas. No final escreva os elementos que foram armazenados em v1 e v2*/
 #include<stdio.h>
 int main(){
 //declaracao de variaveis
-int v[10],v1[10],v2[10];
+int i,v[10],v1[10],v2[10],par[10],impar[10],numero;
+
+for(i=0;i<10;i++){
+printf("Digite um numero para a posicao|%d|: ",i);
+scanf("%d",&v[i]);
+printf("\n");
+numero=v[i]%2;
+
+if(numero==0){
+    v1[i]=v1;
+    par[i]=1;
+}else if (numero>0){
+    v2[i]=v[i];
+    impar[i]=1;
+}
+
+}//final for
+
+for(i=0;i<10;i++){
+
+    if(impar[i]=1){
+    printf("\nNumero impar:%.0d,na posicao:%d",v2[i],i);
+    }
+   
+}//final for
+
+for(i=0;i<10;i++){
+    if(par[i]=1){
+    printf("\nNumero par:%.0d,na posicao:%d",v1[i],i);
+    }
+   
+}
 
 
 }//fim da main
