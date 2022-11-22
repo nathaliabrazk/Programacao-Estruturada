4eb4c7a6b99ef5de5ab9a9482368db5fb74c978d
nathaliabrazk <nathaliabrazk@gmail.com>
2022-11-22 16:08:04 -0300

    .

---
+++ Lista 4-Struct/6-Cadastro.c
@@ -0,0 +1,15 @@
+/*Definir a estrutura cuja representação gráfica é dada a seguir, definir os campos com
+os tipos básicos necessários.
+a) Crie um vetor Cadastro com 5 elementos.
+b) Permita ao usuário entrar com dados para preencher esses 5 cadastros.
+c) Encontre a pessoa com maior idade entre os cadastrados
+d) Encontre as pessoas do sexo masculino
+e) Encontre as pessoas com salário maior que 1000.
+f) Imprima os dados da pessoa cuja identidade seja igual a um valor fornecido pelo
+usuário
+*/
+#include<stdio.h>
+
+int main(){
+
+}
